5cbb2398d5264337e14ba92675b4eb0cbb30b904
JMSams <sams_drum@hotmail.com>
2018-10-12 17:28:07 +0100

    Added dialogue system

---
+++ Creative Project 1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public DialogueNode startNode;
+
+    public TMPro.TextMeshProUGUI dialogueBox;
+
+    DialogueNode currentNode;
+
+    void Start()
+    {
+        TriggerNextDialogue(startNode);
+    }
+
+    public void TriggerNextDialogue(DialogueNode node)
+    {
+        currentNode = node;
+        StartCoroutine(TypeWriter(node.text));
+    }
+    public void TriggerNextDialogue(int choice)
+    {
+        currentNode = currentNode.choices[choice];
+        StartCoroutine(TypeWriter(currentNode.text));
+    }
+
+    IEnumerator TypeWriter(string text, float secondsPerChar = 0.05f)
+    {
+        dialogueBox.text = "";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            dialogueBox.text += text[i];
+            yield return new WaitForSeconds(secondsPerChar);
+        }
+    }
+}
---
+++ Creative Project 1/Assets/Scripts/DialogueNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue Node", menuName = "Dialogue Node", order = 0)]
+public class DialogueNode : ScriptableObject
+{
+    [TextArea(3, 8)]
+    public string text;
+
+    public DialogueNode[] choices;
+}
